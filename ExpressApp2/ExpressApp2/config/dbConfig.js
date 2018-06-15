71c2409fd33fd729336966c92f55222717511eaf
장 상련 <srjang@taihoinst.com>
2018-06-15 14:14:36 +0900

    CC

--- ExpressApp2/ExpressApp2/config/dbConfig.js
+++ ExpressApp2/ExpressApp2/config/dbConfig.js
@@ -11,16 +11,16 @@ var dbConfig = {
     }
 };
 
-var autowayDbConfig = {
-    user: 'taihoinst',
-    password: 'taiho9788!',
-    server: 'taiholab.database.windows.net',
-    database: 'autoway_1',
-    connectionTimeout : 30000,
-    requestTimeout : 30000,
-    options: {
-        encrypt: true
-    }
+//var autowayDbConfig = {
+//    user: 'taihoinst',
+//    password: 'taiho9788!',
+//    server: 'taiholab.database.windows.net',
+//    database: 'autoway_1',
+//    connectionTimeout : 30000,
+//    requestTimeout : 30000,
+//    options: {
+//        encrypt: true
+//    }
 }
 
 module.exports = { dbConfig, autowayDbConfig }
